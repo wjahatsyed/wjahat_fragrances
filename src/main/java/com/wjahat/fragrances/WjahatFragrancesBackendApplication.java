bf02b3a49b82b9923489043f0e6214d52b0efbfb
Wajahat Adil <wajahatsyed1257@gmail.com>
2025-03-29 20:06:39 +0500

    Initial commit - Spring Boot backend setup

---
+++ src/main/java/com/wjahat/fragrances/WjahatFragrancesBackendApplication.java
@@ -0,0 +1,13 @@
+package com.wjahat.fragrances;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WjahatFragrancesBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WjahatFragrancesBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wjahat/fragrances/WjahatFragrancesBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wjahat.fragrances;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WjahatFragrancesBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
